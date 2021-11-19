618b4a83564450a8fe21d581c043ecf543f4a0bf
ayoline <ayo@outlook.com.br>
2021-11-19 09:43:28 -0300

    atualiza exercicios da aula 18-11-21

--- novembro-2021/js/aula-18-11.js
+++ novembro-2021/js/aula-18-11.js
@@ -3,10 +3,10 @@ document.getElementById('age-in-about-me').innerHTML = "37 anos";
 document.getElementById('marital-in-about-me').innerHTML = "Aracajú - SE";
 document.getElementById('img-in-about-me').src = "/images/avatar.png";
 
-document.getElementById('h1-in-my-hobby').innerHTML = "Escalada";
-document.getElementById('p1-in-my-hobby').innerHTML = "O meu maior hobby seguramente é o esporte " +
+document.getElementById('h1-in-my-hobbies').innerHTML = "Escalada";
+document.getElementById('p1-in-my-hobbies').innerHTML = "O meu maior hobby seguramente é o esporte " +
     "conhecido como escalada, de todos os tipos, mas principalmente na rocha.";
-const link = document.getElementById('link-in-my-hobby')
+const link = document.getElementById('link-in-my-hobbies')
 link.innerHTML = "Mais informações...";
 link.href = "https://pt.wikipedia.org/wiki/Escalada";
 link.target = "_target";
