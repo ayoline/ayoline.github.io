6fd475145d0a201d7a30995c863060fe6b9ee92d
ayoline <ayo@outlook.com.br>
2021-11-18 21:27:04 -0300

    atualiza exercicios da aula 18-11-21

--- novembro-2021/js/aula-18-11.js
+++ novembro-2021/js/aula-18-11.js
@@ -1,4 +1,16 @@
-if (true) {
-    console.log("js is loaded...");
-}
+document.getElementById('h1-in-about-me').innerHTML = "Anderson Pacheco Soares";
+document.getElementById('age-in-about-me').innerHTML = "37 anos";
+document.getElementById('marital-in-about-me').innerHTML = "Casado";
+document.getElementById('img-in-about-me').src = "/images/avatar.png";
 
+document.getElementById('h1-in-my-hobby').innerHTML = "Escalada";
+document.getElementById('p1-in-my-hobby').innerHTML = "O meu maior hobby seguramente é o esporte " +
+    "conhecido como escalada, de todos os tipos, mas principalmente na rocha.";
+const link = document.getElementById('link-in-my-hobby')
+link.innerHTML = "Mais informações...";
+link.href = "https://pt.wikipedia.org/wiki/Escalada";
+link.target = "_target";
+document.getElementById('img1-images').src = "../images/aula-18-11/climb1.jpg";
+document.getElementById('img2-images').src = "../images/aula-18-11/climb2.jpg";
+document.getElementById('img3-images').src = "../images/aula-18-11/climb3.jpg";
+document.getElementById('img4-images').src = "../images/aula-18-11/climb4.jpg";
