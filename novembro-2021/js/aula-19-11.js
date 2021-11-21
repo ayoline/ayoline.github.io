72223cb5e2fac5d767a67ad1c4a0b395f6b27caa
ayoline <ayo@outlook.com.br>
2021-11-21 16:16:04 -0300

    atualiza trabalho de 19-11

---
+++ novembro-2021/js/aula-19-11.js
@@ -0,0 +1,16 @@
+document.getElementById('h1-in-about-me').innerHTML = "Anderson Pacheco Soares";
+document.getElementById('age-in-about-me').innerHTML = "37 anos";
+document.getElementById('state-in-about-me').innerHTML = "Aracajú - SE";
+document.getElementById('img-in-about-me').src = "/images/avatar.png";
+
+document.getElementById('image1-in-pearl-jam').src = "../images/aula-19-11/pearl-jam1.jpg";
+document.getElementById('link1-images').href = "https://ayoline.github.io/novembro-2021/images/aula-18-11/climb1.jpg";
+
+document.getElementById('image2-in-pearl-jam').src = "../images/aula-19-11/pearl-jam2.jpg";
+document.getElementById('link2-images').href = "https://ayoline.github.io/novembro-2021/images/aula-18-11/climb2.jpg";
+
+document.getElementById('image3-in-pearl-jam').src = "../images/aula-19-11/pearl-jam3.jpg";
+document.getElementById('link3-images').href = "https://ayoline.github.io/novembro-2021/images/aula-18-11/climb3.jpg";
+
+document.getElementById('image4-in-pearl-jam').src = "../images/aula-19-11/pearl-jam4.jpg";
+document.getElementById('link4-images').href = "https://ayoline.github.io/novembro-2021/images/aula-18-11/climb4.jpg";
