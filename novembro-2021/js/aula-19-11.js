4c9c649b20a009f58d8ce95c369fc67e6129e061
ayoline <ayo@outlook.com.br>
2021-11-21 21:28:02 -0300

    atualiza trabalho de 19-11

--- novembro-2021/js/aula-19-11.js
+++ novembro-2021/js/aula-19-11.js
@@ -1,16 +1,62 @@
+document.getElementById('link-initial-image').href = "#initial-image";
+document.getElementById('link-initial-image').innerHTML = "Página Inicial";
+document.getElementById('link-about-me').href = "#about-me";
+document.getElementById('link-about-me').innerHTML = "About me";
+document.getElementById('link-pearl-jam').href = "#pearl-jam";
+document.getElementById('link-pearl-jam').innerHTML = "Pearl Jam - Info";
+
+
+document.getElementById('p-in-initial-image').innerHTML = "The Seattle Grunge Experience";
+
+
 document.getElementById('h1-in-about-me').innerHTML = "Anderson Pacheco Soares";
 document.getElementById('age-in-about-me').innerHTML = "37 anos";
 document.getElementById('state-in-about-me').innerHTML = "Aracajú - SE";
 document.getElementById('img-in-about-me').src = "/images/avatar.png";
 
 document.getElementById('image1-in-pearl-jam').src = "../images/aula-19-11/pearl-jam1.jpg";
-document.getElementById('link1-images').href = "https://ayoline.github.io/novembro-2021/images/aula-18-11/climb1.jpg";
+document.getElementById('link1-in-images').href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam1.jpg";
+document.getElementById('link1-in-images').target = "_target";
 
 document.getElementById('image2-in-pearl-jam').src = "../images/aula-19-11/pearl-jam2.jpg";
-document.getElementById('link2-images').href = "https://ayoline.github.io/novembro-2021/images/aula-18-11/climb2.jpg";
+document.getElementById('link2-in-images').href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam2.jpg";
+document.getElementById('link2-in-images').target = "_target";
 
 document.getElementById('image3-in-pearl-jam').src = "../images/aula-19-11/pearl-jam3.jpg";
-document.getElementById('link3-images').href = "https://ayoline.github.io/novembro-2021/images/aula-18-11/climb3.jpg";
+document.getElementById('link3-in-images').href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam3.jpg";
+document.getElementById('link3-in-images').target = "_target";
 
 document.getElementById('image4-in-pearl-jam').src = "../images/aula-19-11/pearl-jam4.jpg";
-document.getElementById('link4-images').href = "https://ayoline.github.io/novembro-2021/images/aula-18-11/climb4.jpg";
+document.getElementById('link4-in-images').href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam4.jpg";
+document.getElementById('link4-in-images').target = "_target";
+
+
+document.getElementById('h1-in-info').innerHTML = "About the band";
+document.getElementById('p1-in-box').innerHTML = "Name: pearl-Jam";
+document.getElementById('p2-in-box').innerHTML = "Genres: Grunge - Alternative Rock";
+document.getElementById('p3-in-box').innerHTML = `To get more information, <a id="link-information" href = "https://pt.wikipedia.org/wiki/Pearl_Jam">click here</a>`;
+
+const youtube1 = document.getElementById('link1-youtube');
+youtube1.innerHTML = "Alive";
+youtube1.href = "https://www.youtube.com/watch?v=qM0zINtulhM";
+youtube1.target = "_blank";
+
+const youtube2 = document.getElementById('link2-youtube');
+youtube2.innerHTML = "Black";
+youtube2.href = "https://www.youtube.com/watch?v=5ZH2it92ZmA";
+youtube2.target = "_blank";
+
+const youtube3 = document.getElementById('link3-youtube');
+youtube3.innerHTML = "Even Flow";
+youtube3.href = "https://www.youtube.com/watch?v=CxKWTzr-k6s";
+youtube3.target = "_blank";
+
+const youtube4 = document.getElementById('link4-youtube');
+youtube4.innerHTML = "Crazy Mary";
+youtube4.href = "https://www.youtube.com/watch?v=qsZl9RgueCw";
+youtube4.target = "_blank";
+
+const youtube5 = document.getElementById('link5-youtube');
+youtube5.innerHTML = "Jeremy";
+youtube5.href = "https://www.youtube.com/watch?v=MS91knuzoOA";
+youtube5.target = "_blank";
