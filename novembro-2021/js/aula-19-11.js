71ea7985b58354b0e3bdcf949543f2031c633482
ayoline <ayo@outlook.com.br>
2021-11-21 22:30:15 -0300

    atualiza trabalho de 19-11

--- novembro-2021/js/aula-19-11.js
+++ novembro-2021/js/aula-19-11.js
@@ -1,62 +1,63 @@
-document.getElementById('link-initial-image').href = "#initial-image";
-document.getElementById('link-initial-image').innerHTML = "Página Inicial";
-document.getElementById('link-about-me').href = "#about-me";
-document.getElementById('link-about-me').innerHTML = "About me";
-document.getElementById('link-pearl-jam').href = "#pearl-jam";
-document.getElementById('link-pearl-jam').innerHTML = "Pearl Jam - Info";
+document.querySelectorAll('#titles-in-fixed-navbar a')[0].href = "#initial-image";
+document.querySelectorAll('#titles-in-fixed-navbar a')[0].innerHTML = "Página Inicial";
+document.querySelectorAll('#titles-in-fixed-navbar a')[1].href = "#about-me";
+document.querySelectorAll('#titles-in-fixed-navbar a')[1].innerHTML = "About me";
+document.querySelectorAll('#titles-in-fixed-navbar a')[2].href = "#pearl-jam";
+document.querySelectorAll('#titles-in-fixed-navbar a')[2].innerHTML = "Pearl Jam - Info";
 
 
-document.getElementById('p-in-initial-image').innerHTML = "The Seattle Grunge Experience";
+document.querySelector('#initial-image p').innerHTML = "The Seattle Grunge Experience";
 
 
-document.getElementById('h1-in-about-me').innerHTML = "Anderson Pacheco Soares";
-document.getElementById('age-in-about-me').innerHTML = "37 anos";
-document.getElementById('state-in-about-me').innerHTML = "Aracajú - SE";
-document.getElementById('img-in-about-me').src = "/images/avatar.png";
+document.querySelector('#about-me h1').innerHTML = "Anderson Pacheco Soares";
+document.querySelectorAll('#about-me p')[0].innerHTML = "37 anos";
+document.querySelectorAll('#about-me p')[1].innerHTML = "Aracajú - SE";
+document.querySelector('#about-me img').src = "/images/avatar.png";
 
-document.getElementById('image1-in-pearl-jam').src = "../images/aula-19-11/pearl-jam1.jpg";
-document.getElementById('link1-in-images').href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam1.jpg";
-document.getElementById('link1-in-images').target = "_target";
+document.querySelectorAll('#images-in-pearl-jam img')[0].src = "../images/aula-19-11/pearl-jam1.jpg";
+document.querySelectorAll('#images-in-pearl-jam a')[0].href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam1.jpg";
+document.querySelectorAll('#images-in-pearl-jam a')[0].target = "_target";
 
-document.getElementById('image2-in-pearl-jam').src = "../images/aula-19-11/pearl-jam2.jpg";
-document.getElementById('link2-in-images').href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam2.jpg";
-document.getElementById('link2-in-images').target = "_target";
+document.querySelectorAll('#images-in-pearl-jam img')[1].src = "../images/aula-19-11/pearl-jam2.jpg";
+document.querySelectorAll('#images-in-pearl-jam a')[1].href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam2.jpg";
+document.querySelectorAll('#images-in-pearl-jam a')[1].target = "_target";
 
-document.getElementById('image3-in-pearl-jam').src = "../images/aula-19-11/pearl-jam3.jpg";
-document.getElementById('link3-in-images').href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam3.jpg";
-document.getElementById('link3-in-images').target = "_target";
+document.querySelectorAll('#images-in-pearl-jam img')[2].src = "../images/aula-19-11/pearl-jam3.jpg";
+document.querySelectorAll('#images-in-pearl-jam a')[2].href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam3.jpg";
+document.querySelectorAll('#images-in-pearl-jam a')[2].target = "_target";
 
-document.getElementById('image4-in-pearl-jam').src = "../images/aula-19-11/pearl-jam4.jpg";
-document.getElementById('link4-in-images').href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam4.jpg";
-document.getElementById('link4-in-images').target = "_target";
+document.querySelectorAll('#images-in-pearl-jam img')[3].src = "../images/aula-19-11/pearl-jam4.jpg";
+document.querySelectorAll('#images-in-pearl-jam a')[3].href = "https://ayoline.github.io/novembro-2021/images/aula-19-11/pearl-jam4.jpg";
+document.querySelectorAll('#images-in-pearl-jam a')[3].target = "_target";
 
 
-document.getElementById('h1-in-info').innerHTML = "About the band";
-document.getElementById('p1-in-box').innerHTML = "Name: pearl-Jam";
-document.getElementById('p2-in-box').innerHTML = "Genres: Grunge - Alternative Rock";
-document.getElementById('p3-in-box').innerHTML = `To get more information, <a id="link-information" href = "https://pt.wikipedia.org/wiki/Pearl_Jam">click here</a>`;
+document.querySelector('#info-in-pearl-jam h1').innerHTML = "About the band";
+document.querySelectorAll('#info-in-pearl-jam p')[0].innerHTML = "Name: pearl-Jam";
+document.querySelectorAll('#info-in-pearl-jam p')[1].innerHTML = "Genres: Grunge - Alternative Rock";
+document.querySelectorAll('#info-in-pearl-jam p')[2].innerHTML = `To get more information, <a id="link-information" href = "https://pt.wikipedia.org/wiki/Pearl_Jam">click here</a>`;
+document.querySelector('#info-in-pearl-jam a').target = "_blank";
 
-const youtube1 = document.getElementById('link1-youtube');
+const youtube1 = document.querySelectorAll('#box-in-links a')[0];
 youtube1.innerHTML = "Alive";
 youtube1.href = "https://www.youtube.com/watch?v=qM0zINtulhM";
 youtube1.target = "_blank";
 
-const youtube2 = document.getElementById('link2-youtube');
+const youtube2 = document.querySelectorAll('#box-in-links a')[1];
 youtube2.innerHTML = "Black";
 youtube2.href = "https://www.youtube.com/watch?v=5ZH2it92ZmA";
 youtube2.target = "_blank";
 
-const youtube3 = document.getElementById('link3-youtube');
+const youtube3 = document.querySelectorAll('#box-in-links a')[2];
 youtube3.innerHTML = "Even Flow";
 youtube3.href = "https://www.youtube.com/watch?v=CxKWTzr-k6s";
 youtube3.target = "_blank";
 
-const youtube4 = document.getElementById('link4-youtube');
+const youtube4 = document.querySelectorAll('#box-in-links a')[3];
 youtube4.innerHTML = "Crazy Mary";
 youtube4.href = "https://www.youtube.com/watch?v=qsZl9RgueCw";
 youtube4.target = "_blank";
 
-const youtube5 = document.getElementById('link5-youtube');
+const youtube5 = document.querySelectorAll('#box-in-links a')[4];
 youtube5.innerHTML = "Jeremy";
 youtube5.href = "https://www.youtube.com/watch?v=MS91knuzoOA";
 youtube5.target = "_blank";
