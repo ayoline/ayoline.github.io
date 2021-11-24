01ea620cdf2c32c4db95dea299a1fc8f6afac0a0
ayoline <ayo@outlook.com.br>
2021-11-23 21:38:31 -0300

    Atualiza exercício do dia 23-11-21

--- novembro-2021/js/aula-23-11.js
+++ novembro-2021/js/aula-23-11.js
@@ -1,12 +1,21 @@
-function generateCopyForms() {
+document.querySelector('body h1').innerHTML = "Restaurante Morte Lenta";
+document.querySelector('body h2').innerHTML = "Formulário de satisfação do cliente";
+
+document.querySelectorAll('label')[0].innerHTML = "Nome";
+document.querySelectorAll('label')[1].innerHTML = "Email";
+document.querySelectorAll('label')[2].innerHTML = "Idade";
+document.querySelectorAll('label')[3].innerHTML = "Comentário";
+
+function genCopyForm() {
 
     const nameValue = document.getElementById("name").value;
     const emailValue = document.getElementById("email").value;
     const ageValue = document.getElementById("age").value;
     const commentValue = document.getElementById("comment").value;
 
-    document.getElementById("results").innerHTML = `<p>Nome: ${nameValue}</p>`;
-    console.log(nameValue);
+    if (nameValue) {
+        document.getElementById("results").innerHTML = `<p>Nome: ${nameValue}</p>`;
+    }
     if (emailValue) {
         document.getElementById("results").innerHTML += `<p>Email: ${emailValue}</p>`;
     }
