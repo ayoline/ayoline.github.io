618f507aa08447888d3aeb64aeb5a4fa39bc7c3d
ayoline <ayo@outlook.com.br>
2022-01-13 19:13:13 -0300

    atualiza exercicio 2

--- janeiro-2022/JS/aula-13-01/aula-13-01-exe02.js
+++ janeiro-2022/JS/aula-13-01/aula-13-01-exe02.js
@@ -9,17 +9,16 @@ no console todos os dados que a compõe.
 */
 
 // Exercício 2
-function showArray(el) {
-    let line;
-
-    if (Array.isArray(el)) {
-        line = '[' + el.reduce((acc, element) => {
+function showArray(_el) {
+    let reduced
+    if (Array.isArray(_el)) {
+        reduced = "[" + _el.reduce((acc, element) => {
             return (acc + (showArray(element)));
-        }, "") + ']';
-        return line;
+        }, "") + "]";
+        return reduced;
     }
-    return el;
+    return _el;
 }
 
-const array1 = [[1, 2, 3], ["a", "b", "c", "d"], [4, 5, 6]];
+const array1 = [[1, 2, 3], ["a", "b", "c", "d"], [4, 5, 6, [2, 3, [6, 7]]]];
 console.log(showArray(array1));
