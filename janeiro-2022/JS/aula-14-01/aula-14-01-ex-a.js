272d54d43699476bf887fde62863206ed585c15a
ayoline <ayo@outlook.com.br>
2022-01-14 21:33:44 -0300

    added questions from 14-01

---
+++ janeiro-2022/JS/aula-14-01/aula-14-01-ex-a.js
@@ -0,0 +1,3 @@
+// a. Crie uma função que receba quatro parâmetros e retorne o produto da multiplicação
+// de todos eles. Utilize o spread operator para enviar os itens de um vetor como 
+// parâmetros da sua função;
---
+++ janeiro-2022/JS/aula-14-01/aula-14-01-ex-b.js
@@ -0,0 +1,3 @@
+// b. Crie uma função que receba dois vetores e retorne a concatenação dos dois em um
+// novo vetor. Utilize o spread operator para realizar a concatenação;
+
